c4ff6f0dd1dc68cb734278a179d9633054043c56
Auric Vente <mprops@protonmail.com>
2021-08-16 19:42:39 -0600

    spacing and readme

--- src/numberstring.nim
+++ src/numberstring.nim
@@ -43,26 +43,34 @@ proc numberword*(num: SomeNumber): string =
   if "." in $num:
     let split = numstring(num).split(".")
     return numberword(parseInt(split[0])) & " dot " & numberword(parseInt(split[1]))
+
   let n = int(num)
+
   if n < 0: return "minus " & numberword(-n)
+
   if n < 10:
     return ["zero", "one", "two", "three", "four", "five",
             "six", "seven", "eight", "nine"][n]
+
   if n < 20:
     return ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
             "sixteen", "seventeen", "eighteen", "nineteen"][n - 10]
+
   if n < 100:
     let tens = ["twenty", "thirty", "forty", "fifty", "sixty",
             "seventy", "eighty", "ninety"][(int(n / 10) - 2) mod 10]
+
     if n mod 10 != 0:
       return tens & "-" & numberword(n mod 10)
     else:
       return tens
+
   if n < 1000:
     if n mod 100 == 0:
       return numberword(int(n / 100)) & " hundred"
     else:
       return numberword(int(n / 100)) & " hundred and " & numberword(n mod 100)
+
   let powers = [("thousand", 3), ("million", 6),
         ("billion", 9), ("trillion", 12), ("quadrillion", 15),
         ("quintillion", 18), ("sextillion", 21), ("septillion", 24),
@@ -72,15 +80,20 @@ proc numberword*(num: SomeNumber): string =
         ("sexdecillion", 51), ("eptendecillion", 54),
         ("octadecillion", 57), ("novemdecillion", 61),
         ("vigintillion", 64)]
+
   let ns = $n
   var idx = powers.len - 1
+
   while true:
     let d = powers[idx][1]
+
     if ns.len > d:
       let first = numberword(parseInt(ns[0..^(d + 1)]))
       let second = numberword(parseInt(ns[^d..^1]))
+      
       if second == "zero":
         return first & " " & powers[idx][0]
       else:
         return first & " " & powers[idx][0] & " " & second
+
     idx = idx - 1
